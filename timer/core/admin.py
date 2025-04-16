d4fb48306916dba4efe1c9d7c4e5511d1d6f03be
mrkaplan0 <101001725+mrkaplan0@users.noreply.github.com>
2025-04-16 13:10:30 +0200

    We have note model

--- timer/core/admin.py
+++ timer/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from core.models import *
 # Register your models here.
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['id','date','note','priority']
+
+    class Meta:
+        model = Note
--- timer/core/models.py
+++ timer/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    date = models.DateField()
+    note = models.TextField(default='')
+    priority = models.IntegerField()
+
+    def __str__(self):
+        return self.note
