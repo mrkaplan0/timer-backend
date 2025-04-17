ab8803ce19ac313f39d4ffe9d1088f96a5c479be
Michael Böckle <michiboeckle0101@gmail.com>
2025-04-17 08:18:38 +0200

    configure database settings
    -add new config for postgresql in settings.py debug report view

--- timer/timer/settings.py
+++ timer/timer/settings.py
@@ -79,15 +79,16 @@ WSGI_APPLICATION = 'timer.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DBtest'),  # Liest die Umgebungsvariable DATABASE_NAME aus .env
+        'USER': env('admin'),    # Liest die Umgebungsvariable DATABASE_USER aus .env
+        'PASSWORD': env('r8%x-81q(cr-5=he)tw=p%lpm%l+^*2jz92v*%26tog@bkumq8'), # Liest die Umgebungsvariable DATABASE_PASSWORD aus .env
+        'HOST': env('localhost', default='localhost'), # read database host 
+        'PORT': env('5432', default='5432'),   # read port
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
